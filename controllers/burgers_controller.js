fc5cb881516ea3257b776c801457fab8108015dd
Doug Davidoff <douglass.davidoff@gmail.com>
2021-01-27 14:29:26 -0500

    Adding burgers_controller.js file

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,37 @@
+var express = require("express");
+var router = express.Router();
+
+var burger = require("../models/burger");
+
+router.get("/", (req, res) => {
+
+    burger.selectAll(data => {
+        var hbsObject = {
+            burger: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("/api/burger", (req, res) => {
+    burger.insertOne("burger_name", req.body.burger_name, result => {
+        res.json({id: result.insertId});
+    });
+});
+
+router.put("/api/burger/:id", (req, res) => {
+
+    var burgerId = req.params.id;
+
+    burger.updateOne(burgerId, result => {
+        if (result.changeRows == 0) {
+
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+module.exports = router;
