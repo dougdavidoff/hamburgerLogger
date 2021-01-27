c5c128af014ec85513a9354a5632f6a684fe0f0e
Doug Davidoff <douglass.davidoff@gmail.com>
2021-01-27 15:20:27 -0500

    adding burger.js

---
+++ public/assets/js/burger.js
@@ -0,0 +1,35 @@
+$(function () {
+    $(".change-devour").on("click", function (event) {
+        var id = $(this).data("id");
+
+        var newState = {
+            devoured: true
+        };
+
+
+        $.ajax("/api/burger/" + id, {
+            type: "PUT",
+            data: newState
+        }).then(
+            function () {
+                console.log("changed devoured to true");
+
+                location.reload();
+            }
+        );
+    });
+    $(".burgerInput").on("submit", function(event) {
+        event.preventDefault();
+        var newBurger = {
+            burger_name: $("#newBurger").val().trim()
+        };
+
+        $.ajax("/api/burger", {
+            type: "POST",
+            data: newBurger
+        }).then(function() {
+            console.log("new burger created");
+            location.reload();
+        })
+    })
+})
