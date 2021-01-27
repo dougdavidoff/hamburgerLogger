f240ece37579c9a063145706f04bc10ce31df2f6
Doug Davidoff <douglass.davidoff@gmail.com>
2021-01-27 11:02:37 -0500

    Adding orm.js

---
+++ config/orm.js
@@ -0,0 +1,36 @@
+var connection = require("./connection");
+
+var orm = {
+    selectAll: (table, cb) => {
+
+        var queryString = "SELECT * FROM " + table + ";";
+        connection.query(queryString, (err, result) => {
+            if (err) {
+                throw err;
+            }
+            cb(results);
+        });
+    },
+    insertOne: (table, column, value, cb) => {
+
+
+
+        var queryString = "INSERT INTO ?? (??) VALUES (?)";
+        connection.query(queryString, [table, column, value], (err, res) => {
+            if(err) throw err;
+
+            cb(res);
+        });
+    },
+    updateOne: (table, column, burgerId, cb) => {
+        var queryString = "UPDATE ?? SET ?? = 1 WHERE id = ?";
+        connection.query(queryString, [table, column, burgerId], (err, res) => {
+            console.log("update" + queryString);
+            if (err) throw err;
+            console.log(res);
+            cb(res);
+    });
+    }
+}
+
+module.exports = orm;
